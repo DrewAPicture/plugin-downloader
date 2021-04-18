4efc03aa7ffbac1b4bc46a10b26c000a04bcf3a2
Drew Jaynes <drew@affiliatewp.com>
2021-04-18 01:36:16 -0600

    Clean up main plugin file.

--- plugin-downloader.php
+++ plugin-downloader.php
@@ -1,15 +1,11 @@
 <?php
 /**
  * Plugin Name:     Plugin Downloader
- * Plugin URI:      PLUGIN SITE HERE
  * Description:     Makes it possible to download any plugins installed on a WordPress site.
  * Author:          Drew Jaynes
  * Author URI:      https://werdswords.com
  * Text Domain:     plugin-downloader
  * Domain Path:     /languages
  * Version:         0.1.0
- *
- * @package         Plugin_Downloader
  */
 
-// Your code starts here.
